d4da8277eb08431ee543422f037acebade85d9b2
phenotypic <33377034+phenotypic@users.noreply.github.com>
2019-07-19 20:53:00 +0200

    Update NodeMCU-Modulating-Valve.ino

--- examples/NodeMCU-Modulating-Valve.ino
+++ examples/NodeMCU-Modulating-Valve.ino
@@ -12,7 +12,7 @@
 const char* ssid = "SSID"; //Name of your network
 const char* password = "PASSWORD"; //Password for your network
 const char* relay = "HIGH"; //Relay type (`HIGH` or `LOW`)
-const char* mdns = "relay"; //mDNS name
+const char* mdns = "valve"; //mDNS name
 const uint32_t modulationOn = 10000; //Time (in ms) for relay to be ON when modulating
 const uint32_t modulationOff = 20000; //Time (in ms) for relay to be OFF when modulating
 //////////////////////////////////////////////////////////////
